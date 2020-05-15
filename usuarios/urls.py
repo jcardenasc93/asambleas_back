37bd47fa62280f8e84680948b70199f61e67e194
jcardenascAndes93 <j.cardenasc@uniandes.edu.co>
2020-05-15 10:23:00 -0500

    Crea vista para listar usuarios Asambleistas y se crea endpoint para crear usuario Asambleista. Se validan permisos y autenticaciones de equipo staff

--- usuarios/urls.py
+++ usuarios/urls.py
@@ -4,5 +4,7 @@ from . import views
 
 urlpatterns = [    
     path('api/v1/new_asam/', views.createUser),
+    path('api/v1/asambleistas/', views.ListAsambleistasView.as_view({'get': 'list'}), name='list_users'),
+    path('api/v1/asambleistas/nuevo', views.ListAsambleistasView.as_view({'post': 'create'}), name='crea_asambleista'),
     path('api/v1/auth/', include('djoser.urls.authtoken'))
 ]
--- usuarios/views.py
+++ usuarios/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render
-from rest_framework.decorators import api_view, permission_classes
+from rest_framework.decorators import api_view, permission_classes, authentication_classes
+from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework import viewsets
 
 from .models import Asambleista
+from .serializers import AsambleistaSerializer
 # Create your views here.
 
 
@@ -22,3 +25,64 @@ def createUser(request):
     except:
         return Response({'detail': 'No se pudo crear el usuario {}. Verifique que no este duplicado'.format(asambleista.username)},
                         status=status.HTTP_409_CONFLICT)
+
+
+class ListAsambleistasView(viewsets.ModelViewSet):
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (IsAuthenticated,)
+    serializer_class = AsambleistaSerializer
+
+    def get_queryset(self):
+        if self.request.user.is_staff:
+            return Asambleista.objects.all()
+        else:
+            return Response({"detail": "Acceso denegado. Autentiquese como usuario administrador"}, status=status.HTTP_401_UNAUTHORIZED)
+
+    def create(self, request, *args, **kwargs):
+        if self.request.user.is_staff:
+            serializer = self.get_serializer(data=request.data)
+            serializer.is_valid(raise_exception=True)
+            self.perform_create(serializer)
+            headers = self.get_success_headers(serializer.data)
+            return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+        else:
+            return Response({"detail": "Acceso denegado. Autentiquese como usuario administrador"}, status=status.HTTP_401_UNAUTHORIZED)
+
+#
+    # def retrieve(self, request, pk=None):
+    #    asambleista = get_object_or_404(Asambleista, id=pk)
+    #    # check if request.user is owner
+    #    if concurso.owner == self.request.user:
+    #        videos = UserVideo.objects.order_by(
+    #            '-upload_date').filter(concurso=pk)
+    #        concurso_serializer = ConcursoSerializer(concurso)
+    #        videos_serializer = VideoSerializer(videos, many=True)
+    #        return Response({'concurso': concurso_serializer.data, 'videos': videos_serializer.data})
+#
+    #    else:
+    #        return Response({'response': 'Owner unauthorized'}, status=status.HTTP_401_UNAUTHORIZED)
+#
+    # def update(self, request, pk=None, **kwargs):
+    #    concurso = get_object_or_404(Concurso, id=pk)
+    #    # check if request.user is owner
+    #    if concurso.owner == self.request.user:
+    #        partial = kwargs.pop('partial', False)
+    #        serializer = ConcursoSerializer(
+    #            concurso, data=request.data, partial=partial)
+    #        serializer.is_valid(raise_exception=True)
+    #        self.perform_update(serializer)
+    #        return Response(serializer.data)
+#
+    #    else:
+    #        return Response({'detail': 'Owner unauthorized'}, status=status.HTTP_401_UNAUTHORIZED)
+#
+    # def destroy(self, request, pk):
+    #    concurso = get_object_or_404(Concurso, id=pk)
+    #    # check if request.user is owner
+    #    if concurso.owner == self.request.user:
+    #        self.perform_destroy(concurso)
+    #        return Response({'detail': 'Object deleted'}, status=status.HTTP_204_NO_CONTENT)
+#
+    #    else:
+    #        return Response({'response': 'Owner unauthorized'}, status=status.HTTP_401_UNAUTHORIZED)
+#
