95f993984a7390a10e82044f9ec7b5e71337945f
jcardenascAndes93 <j.cardenasc@uniandes.edu.co>
2020-05-15 10:40:05 -0500

    Agrega endopoints para actualizar y eliminar usuarios asambleistas

--- usuarios/urls.py
+++ usuarios/urls.py
@@ -6,5 +6,7 @@ urlpatterns = [
     path('api/v1/new_asam/', views.createUser),
     path('api/v1/asambleistas/', views.ListAsambleistasView.as_view({'get': 'list'}), name='list_users'),
     path('api/v1/asambleistas/nuevo', views.ListAsambleistasView.as_view({'post': 'create'}), name='crea_asambleista'),
+    path('api/v1/asambleistas/actualizar/<int:pk>', views.ListAsambleistasView.as_view({'patch': 'partial_update'}), name='actualiza_asambleista'),
+    path('api/v1/asambleistas/eliminar/<int:pk>', views.ListAsambleistasView.as_view({'post': 'destroy'}), name='elimina_asambleista'),    
     path('api/v1/auth/', include('djoser.urls.authtoken'))
 ]
--- usuarios/views.py
+++ usuarios/views.py
@@ -5,6 +5,7 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework import viewsets
+from django.shortcuts import get_object_or_404
 
 from .models import Asambleista
 from .serializers import AsambleistaSerializer
@@ -33,12 +34,14 @@ class ListAsambleistasView(viewsets.ModelViewSet):
     serializer_class = AsambleistaSerializer
 
     def get_queryset(self):
+        # check if request.user is staff
         if self.request.user.is_staff:
             return Asambleista.objects.all()
         else:
             return Response({"detail": "Acceso denegado. Autentiquese como usuario administrador"}, status=status.HTTP_401_UNAUTHORIZED)
 
     def create(self, request, *args, **kwargs):
+        # check if request.user is staff
         if self.request.user.is_staff:
             serializer = self.get_serializer(data=request.data)
             serializer.is_valid(raise_exception=True)
@@ -47,42 +50,36 @@ class ListAsambleistasView(viewsets.ModelViewSet):
             return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
         else:
             return Response({"detail": "Acceso denegado. Autentiquese como usuario administrador"}, status=status.HTTP_401_UNAUTHORIZED)
-
-#
     # def retrieve(self, request, pk=None):
-    #    asambleista = get_object_or_404(Asambleista, id=pk)
-    #    # check if request.user is owner
-    #    if concurso.owner == self.request.user:
-    #        videos = UserVideo.objects.order_by(
-    #            '-upload_date').filter(concurso=pk)
-    #        concurso_serializer = ConcursoSerializer(concurso)
-    #        videos_serializer = VideoSerializer(videos, many=True)
-    #        return Response({'concurso': concurso_serializer.data, 'videos': videos_serializer.data})
-#
-    #    else:
-    #        return Response({'response': 'Owner unauthorized'}, status=status.HTTP_401_UNAUTHORIZED)
-#
-    # def update(self, request, pk=None, **kwargs):
-    #    concurso = get_object_or_404(Concurso, id=pk)
-    #    # check if request.user is owner
-    #    if concurso.owner == self.request.user:
-    #        partial = kwargs.pop('partial', False)
-    #        serializer = ConcursoSerializer(
-    #            concurso, data=request.data, partial=partial)
-    #        serializer.is_valid(raise_exception=True)
-    #        self.perform_update(serializer)
-    #        return Response(serializer.data)
-#
-    #    else:
-    #        return Response({'detail': 'Owner unauthorized'}, status=status.HTTP_401_UNAUTHORIZED)
-#
-    # def destroy(self, request, pk):
-    #    concurso = get_object_or_404(Concurso, id=pk)
-    #    # check if request.user is owner
-    #    if concurso.owner == self.request.user:
-    #        self.perform_destroy(concurso)
-    #        return Response({'detail': 'Object deleted'}, status=status.HTTP_204_NO_CONTENT)
-#
-    #    else:
-    #        return Response({'response': 'Owner unauthorized'}, status=status.HTTP_401_UNAUTHORIZED)
-#
+    #   asambleista = get_object_or_404(Asambleista, id=pk)
+    #   # check if request.user is owner
+    #   if concurso.owner == self.request.user:
+    #       videos = UserVideo.objects.order_by(
+    #           '-upload_date').filter(concurso=pk)
+    #       concurso_serializer = ConcursoSerializer(concurso)
+    #       videos_serializer = VideoSerializer(videos, many=True)
+    #       return Response({'concurso': concurso_serializer.data, 'videos': videos_serializer.data})
+    #   else:
+    #       return Response({'response': 'Owner unauthorized'}, status=status.HTTP_401_UNAUTHORIZED)
+
+    def update(self, request, pk=None, **kwargs):
+        asambleista = get_object_or_404(Asambleista, id=pk)
+        # check if request.user is staff
+        if self.request.user.is_staff:
+            partial = kwargs.pop('partial', False)
+            serializer = AsambleistaSerializer(
+                asambleista, data=request.data, partial=partial)
+            serializer.is_valid(raise_exception=True)
+            self.perform_update(serializer)
+            return Response(serializer.data)
+        else:
+            return Response({"detail": "Acceso denegado. Autentiquese como usuario administrador"}, status=status.HTTP_401_UNAUTHORIZED)
+
+    def destroy(self, request, pk):
+        asambleista = get_object_or_404(Asambleista, id=pk)
+        # check if request.user is staff
+        if self.request.user.is_staff:
+            self.perform_destroy(asambleista)
+            return Response({'detail': 'Usuario eliminado'}, status=status.HTTP_204_NO_CONTENT)
+        else:
+            return Response({"detail": "Acceso denegado. Autentiquese como usuario administrador"}, status=status.HTTP_401_UNAUTHORIZED)
