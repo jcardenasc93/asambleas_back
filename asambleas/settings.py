0702ef1b18d4221b5f773581232ab605ebd36384
jcardenascAndes93 <j.cardenasc@uniandes.edu.co>
2020-05-15 02:26:30 -0500

    Agrega HttpResponses y validacion de autenticacion para poder crear un usuario

--- asambleas/settings.py
+++ asambleas/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'phonenumber_field',
+    'rest_framework',
     'djoser',
     'rest_framework.authtoken',
 
@@ -139,4 +140,7 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.TokenAuthentication',
         'rest_framework.authentication.SessionAuthentication',
     ),
+    #'DEFAULT_PERMISSION_CLASSES': [
+    #    'rest_framework.permissions.IsAuthenticated',
+    #]
 }
--- usuarios/views.py
+++ usuarios/views.py
@@ -1,10 +1,23 @@
 from django.shortcuts import render
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework import status
+
 from .models import Asambleista
 # Create your views here.
 
 
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
 def createUser(request):
-    asambleista = Asambleista(username='asambleista_test', first_name='Don asambleista Test',
+    asambleista = Asambleista(username='asam_test', first_name='Don asambleista Test',
                               email='asambleista@asambleista.com', inmueble='INT10 AP 301', documento='1032555678')
     asambleista.set_password('asambleas2020')
-    asambleista.save()
+    try:
+        asambleista.save()
+        return Response({'detail': 'Usuario {} creado correctamente'.format(asambleista.username)},
+                        status=status.HTTP_201_CREATED)
+    except:
+        return Response({'detail': 'No se pudo crear el usuario {}. Verifique que no este duplicado'.format(asambleista.username)},
+                        status=status.HTTP_409_CONFLICT)
