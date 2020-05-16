9441e1ecc8c369b08671fd4bbbeed981780cfce8
jcardenascAndes93 <j.cardenasc@uniandes.edu.co>
2020-05-16 10:52:48 -0500

    Agrega validacion de existencia de archivo excel para el evento relacionado

--- usuarios/views.py
+++ usuarios/views.py
@@ -15,61 +15,66 @@ from .serializers import AsambleistaSerializer
 from eventos.models import Evento
 # Create your views here.
 
+
 def random_password():
-    chars ="abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%^&*()_+}{"
-    password=""
+    chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%^&*()_+}{"
+    password = ""
     for i in range(10):
         password += random.choice(chars)
     return password
+
 
 @api_view(['GET'])
 @permission_classes([IsAuthenticated])
 def createUser(request, pk=None):
     # Lectura del archivo de Excel
     BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-    evento = Evento.objects.get(pk=pk)    
-    nombre_archivo = str(evento.documento_excel)    
-    excel_file = BASE_DIR + "/media/" + nombre_archivo
-    wb = openpyxl.load_workbook(excel_file)
-    worksheet = wb["Sheet1"]
+    evento = Evento.objects.get(pk=pk)
+    nombre_archivo = str(evento.documento_excel)
+    # Valida si existe archivo para el evento
+    if nombre_archivo:
+        excel_file = BASE_DIR + "/media/" + nombre_archivo
+        wb = openpyxl.load_workbook(excel_file)
+        worksheet = wb["Sheet1"]
 
-    # Convertimos archivo en una lista
-    excel_data = list()
-    for row in worksheet.iter_rows():
-        row_data = list()
-        for cell in row:
-            row_data.append(str(cell.value))
-        excel_data.append(row_data)
-    excel_data.pop(0)
+        # Convertimos archivo en una lista
+        excel_data = list()
+        for row in worksheet.iter_rows():
+            row_data = list()
+            for cell in row:
+                row_data.append(str(cell.value))
+            excel_data.append(row_data)
+        excel_data.pop(0)
 
-    usuarios_no_creados = []
-    for data in excel_data:
-        asambleista = ''
-        username = data[2] + '_' + data[0].replace(' ', '_').lower()
-        if data[6] == 'si':
-            mora = True
-        else:
-            mora = False
-        asambleista = Asambleista(inmueble=data[0], first_name=data[1],
-                                    documento=data[2], email=data[3], celular=data[4], coeficiente=data[5], 
-                                    mora=mora, username=username, evento_id=pk)
-        asambleista.set_password(random_password())
-        try:
-            asambleista.save()
-            # TODO: Enviar correo
+        usuarios_no_creados = []
+        for data in excel_data:
+            asambleista = ''
+            username = data[2] + '_' + data[0].replace(' ', '_').lower()
+            if data[6] == 'si':
+                mora = True
+            else:
+                mora = False
+            asambleista = Asambleista(inmueble=data[0], first_name=data[1],
+                                      documento=data[2], email=data[3], celular=data[4], coeficiente=data[5],
+                                      mora=mora, username=username, evento_id=pk)
+            asambleista.set_password(random_password())
+            try:
+                asambleista.save()
+                # TODO: Enviar correo
 
-        except:
-            usuarios_no_creados.append(data[0])
-            pass
-            
-    if len(usuarios_no_creados) > 0:
-        return Response({'usuarios_no_creados': usuarios_no_creados},
+            except:
+                usuarios_no_creados.append(data[0])
+                pass
+
+        if len(usuarios_no_creados) > 0:
+            return Response({'usuarios_no_creados': usuarios_no_creados},
                             status=status.HTTP_206_PARTIAL_CONTENT)
+        else:
+            return Response({'detail': 'Todos los usuarios se crearon correctamente'},
+                            status=status.HTTP_201_CREATED)
+
     else:
-        return Response({'detail': 'Todos los usuarios se crearon correctamente'},
-                           status=status.HTTP_201_CREATED)
-
-
+        return Response({'detail': 'No existe un archivo excel asociado al evento'}, status=status.HTTP_400_BAD_REQUEST)
 class ListAsambleistasView(viewsets.ModelViewSet):
     authentication_classes = (TokenAuthentication,)
     permission_classes = (IsAuthenticated,)
