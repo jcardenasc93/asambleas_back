e3c2cb2fd21116efa09ab0aa9530fcc4efc5598c
jcardenascAndes93 <j.cardenasc@uniandes.edu.co>
2020-05-16 01:37:59 -0500

    Fix settings para heroku

--- asambleas/settings.py
+++ asambleas/settings.py
@@ -87,17 +87,19 @@ AUTH_USER_MODEL = "usuarios.Usuario"
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#        'NAME': os.environ['DB_NAME'],
+#        'USER': os.environ['DB_USER'],
+#        'PASSWORD': os.environ['DB_PASS'],
+#        'HOST': os.environ['DB_HOST'],
+#        'PORT': os.environ['DB_PORT'],
+#    }
+#}
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': os.environ['DB_NAME'],
-        'USER': os.environ['DB_USER'],
-        'PASSWORD': os.environ['DB_PASS'],
-        'HOST': os.environ['DB_HOST'],
-        'PORT': os.environ['DB_PORT'],
-    }
+    'default': {os.environ['DATABASE_URL']}
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
