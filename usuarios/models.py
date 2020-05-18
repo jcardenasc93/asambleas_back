4220f2b22f1de762f81810002864931d4ab5261b
jcardenascAndes93 <j.cardenasc@uniandes.edu.co>
2020-05-18 09:26:43 -0500

    Arregal bug despues del post de creacion de un usuario admin

--- usuarios/models.py
+++ usuarios/models.py
@@ -15,15 +15,19 @@ class Usuario(AbstractUser):
 class Asambleista(Usuario):
     inmueble = models.CharField(max_length=80, blank=True, null=True)
     coeficiente = models.DecimalField(
-        max_digits=10, decimal_places=3, blank=True, null=True)    
+        max_digits=10, decimal_places=3, blank=True, null=True)
     documento = models.CharField(max_length=20, blank=True, null=True)
     celular = PhoneNumberField(blank=True, null=True)
     mora = models.BooleanField(default=False)
-    evento = models.ForeignKey(Evento, on_delete=models.CASCADE, blank=True, null=True)
+    evento = models.ForeignKey(
+        Evento, on_delete=models.CASCADE, blank=True, null=True)
 
     class Meta:
         verbose_name = 'asambleista'
         verbose_name_plural = 'asambleistas'
 
     def __str__(self):
-        return self.inmueble + ' - ' + self.first_name
+        if self.is_staff:
+            return self.username
+        else:
+            return self.inmueble + ' - ' + self.first_name
