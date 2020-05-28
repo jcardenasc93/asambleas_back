dbe5e0cdd96d7f17418635be55ca6970a05d4cba
jcardenascAndes93 <j.cardenasc@uniandes.edu.co>
2020-05-28 13:42:22 -0500

    Agrega campos de presentacion en opciones multiple y el booleano de numeracion en preguntas

--- eventos/models.py
+++ eventos/models.py
@@ -18,23 +18,29 @@ class Evento(models.Model):
     @property
     def filename(self):
         return os.path.basename(self.documento_excel.name)
+
     def __str__(self):
         return self.nombre
+
 
 class Pregunta(models.Model):
     enunciado = models.CharField(max_length=500, null=False)
     activa = models.BooleanField(default=True)
-    evento = models.ForeignKey(Evento, on_delete=models.CASCADE, related_name='evento')
+    evento = models.ForeignKey(
+        Evento, on_delete=models.CASCADE, related_name='evento')
     bloquea_mora = models.BooleanField(default=False)
+    numeracion = models.BooleanField(default=False)
+
 
 class PreguntaAbierta(Pregunta):
     def __str__(self):
         return self.evento.nombre + '-- ' + self.enunciado
 
+
 class PreguntaDecimal(Pregunta):
     minimo = models.IntegerField(verbose_name='valor_minimo')
     maximo = models.IntegerField(verbose_name='valor_maximo')
-    
+
     def __str__(self):
         return self.evento.nombre + '-- ' + self.enunciado
 
@@ -42,15 +48,15 @@ class PreguntaDecimal(Pregunta):
 class PreguntaMultiple(Pregunta):
     puntajeCoeficiente = models.BooleanField(default=False)
     esMultipleResp = models.BooleanField(default=False)
-    respuestasPermitidas =  models.IntegerField(blank=False, default=1)
+    respuestasPermitidas = models.IntegerField(blank=False, default=1)
     strictMax = models.BooleanField(default=False)
-    # TODO: Agregar booleano de cumplimiento con la cantidad de respuestas permitidas
+    opPresentacion = models.IntegerField()
 
     def __str__(self):
         return self.evento.nombre + '-- ' + self.enunciado
+
 
 class OpcionesMultiple(models.Model):
     opcion = models.CharField(max_length=500)
     preguntaSeleccionMultiple = models.ForeignKey(
         PreguntaMultiple, on_delete=models.CASCADE, related_name='opciones')
-
