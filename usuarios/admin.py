7eafc55ca2d821d4ca6f85f0e5e5ec2f1ace1a5a
jcardenasc93 <jcardenasc93@gmail.com>
2020-06-10 22:49:51 -0500

    Agrega vista de usuarios admin al admin de Django

--- usuarios/admin.py
+++ usuarios/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Asambleista, Apoderado
+from .models import Asambleista, Apoderado, AbstractUser
 # Register your models here.
-models = [Asambleista, Apoderado]
+models = [Asambleista, Apoderado, AbstractUser]
 admin.site.register(models)
